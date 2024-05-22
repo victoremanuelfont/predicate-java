fa5cd5e9425af0de71d1f9f80330eb2f1e190193
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-05-22 15:14:42 -0300

    Terceira versão: Reference method com método estático

--- src/entities/Product.java
+++ src/entities/Product.java
@@ -29,6 +29,11 @@ public class Product {
 		this.price = price;
 	}
 
+	public static boolean staticProductPredicate(Product p) {
+		return p.getPrice() >=100.0;
+	}
+	
+	
 	@Override
 	public String toString() {
 		return "Product [name=" + name + ", price=" + price + "]";
