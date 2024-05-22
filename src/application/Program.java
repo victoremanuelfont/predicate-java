2cabad9e82dee0a3ccf1fccca20d0e484cfd87cc
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-05-22 15:23:20 -0300

    Expressão lambda inline

--- src/application/Program.java
+++ src/application/Program.java
@@ -19,8 +19,8 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-		Predicate<Product> pred = p -> p.getPrice() >= 100.0;
-		list.removeIf(pred);
+
+		list.removeIf(p -> p.getPrice() >= 100.0);
 		
 		for(Product p: list) {
 			System.out.println(p);
